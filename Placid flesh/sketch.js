5857655669a44522c1e8490b01f46c34f6faca89
MariaJRojas <MariaJuliana@172-16-22-198.DYNAPOOL.NYU.EDU>
2017-02-28 14:09:19 -0500

    Part 1
    So I decided to go with the adjective international, I’m currently
    playing around with flag colors and organizing the shapes, I think I
    would want to make them move after, but I’m not entirely sure what I
    would want it to do yet.

---
+++ Placid flesh/sketch.js
@@ -0,0 +1,70 @@
+function setup() { 
+  createCanvas(1200, 700);
+} 
+
+function draw() { 
+  background(0);
+  
+  //adjective-international
+  
+  //France
+	noStroke(0);
+	fill(40,9,255);
+  rect(20,20,50,50);
+	
+  noStroke(0);
+  fill(255);
+  rect(40,40,50,50);
+  
+  noStroke(0);
+  fill(255,14,4);
+  rect(60,60,50,50);
+  
+  //India
+  //orange
+  noStroke(0);
+  fill(255,138,58);
+  ellipse(40,200,50,50)
+  
+//green 
+    noStroke(0);
+  fill(18,94,0);
+  ellipse(80,200,50,50);
+  
+  //white 
+  noStroke(0);
+  fill(255,255,255,100);
+  ellipse(60,200,50,50);
+  
+  noStroke(0);
+  fill(18,0,150);
+  ellipse(60,200,25,25);
+  
+  noStroke(0);
+  fill(255,255,255,200);
+  ellipse(60,200,18,18);
+  
+  //Colombia
+  //yellow
+  noStroke(0);
+  fill(255,213,0);
+  rect(200,100,100,50);
+  //blue
+  noStroke(0);
+  fill(40,9,255);
+  rect(230,150,100,40);
+  //red
+  noStroke(0);
+  fill(255,14,4);
+  rect(200,170,100,40);
+  
+  
+
+  
+  
+  
+	
+	
+	
+	
+}
